ecc24ad011cad40bf3321eb491f315998b917fdb
Lourdes1514 <lourdes.pampa@tecsup.edu.pe>
2019-10-21 15:49:19 -0500

    Agregar archivos de proyecto.

---
+++ Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Lab10.Views;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CommandDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InteractiveDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Operaciones());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SimpleDemo());
+            };
+    }
+    }
+}
+
+
